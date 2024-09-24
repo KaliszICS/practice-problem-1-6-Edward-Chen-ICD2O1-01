a5d9c331b09133a58d3560383679befc466bf2c0
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-09-24 13:56:44 +0000

    32

--- PP1_6.py
+++ PP1_6.py
@@ -10,7 +10,7 @@ def q1():
   num1 = input("Input a number: ")
   num2 = input("Input a number: ")
   
-  Print(num + num2)
+  print(f{num1 + num2})
 
 # From input recieve two integers.  Output the quotient rounded down.
 
@@ -23,7 +23,7 @@ def q2():
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print (hello Mr. Kalisz have you seen my work yet?)
+  print ("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
@@ -32,7 +32,7 @@ def q4():
   input("Input a number: ")
   num2 = input("Input a number: ")
   
-  print (int(num1 * num2))
+  print (int(num2))*num2
 
 q1()
 q2()
